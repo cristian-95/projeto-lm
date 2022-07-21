c19703bb75f336ab9f4f70dcd7051fd20d0cef36
cristian-95 <nem.cristianrc@gmail.com>
2022-07-21 19:24:21 -0300

    teste de retorno de um numero - OK

--- projeto_lm_nasm.asm
+++ projeto_lm_nasm.asm
@@ -1,22 +1,24 @@
-/* PROJETO FINAL DE LINGUAGENS DE MONTAGEM - 2022
- * Grupo 3: 4(A x B) retornando o maior valor da diagonal principal.
- *  GABRIEL FRANCISCO HABERMANN
- *  CRISTIAN SANTOS DE CASTRO
- *  IGNACIO ALFREDO SAVI GUALCO
- */
+;*********************************************************
+; PROJETO FINAL DE LINGUAGENS DE MONTAGEM - 2022
+; Grupo 3: 4(A x B) retornando o maior valor da diagonal principal.
+;  GABRIEL FRANCISCO HABERMANN
+;  CRISTIAN SANTOS DE CASTRO
+;  IGNACIO ALFREDO SAVI GUALCO
+;**********************************************************
 
     SECTION .text
     global projeto_lm_nasm
 
 
 ;; teste primario para retornar apenas o tamanho da matriz multiplicado por 2:
-%define     L       dword[ebp+8]
 
-dois        EQU  2
 
 projeto_lm_nasm:
-    push    ebp
-    mov     eax,L
-    mov     ebx,dois
-    mul     ebx
-    pop     ebp
+        push ebp
+        mov ebp,esp
+        mov eax,[ebp+8]
+        add eax,eax
+        mov esp,ebp
+        pop ebp
+        ret
+        ; estrutura copiada do exemplo sum.asm da AULA 8
